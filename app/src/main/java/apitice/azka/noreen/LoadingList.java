84065ecb7d6d781a03038f3a099251e1fe8c1a7b
Azka Noreen <azka.noreen6@gmail.com>
2022-10-01 15:22:29 +0500

    Toolbar Title changed and loading addded

--- app/src/main/java/apitice/azka/noreen/LoadingList.java
+++ app/src/main/java/apitice/azka/noreen/LoadingList.java
@@ -1,14 +1,24 @@
 package apitice.azka.noreen;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.drawerlayout.widget.DrawerLayout;
 
 import android.os.Bundle;
 
-public class LoadingList extends AppCompatActivity {
+import com.google.android.material.navigation.NavigationView;
 
+public class LoadingList extends AppCompatActivity {
+    Toolbar toolba;
+    DrawerLayout drawerLayout;
+    NavigationView navigationView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_loading_list);
+
+        toolba=findViewById(R.id.toolba);
+        toolba.setTitle("Azka Noreen");
+        setSupportActionBar(toolba); //set toolbar to act as action bar
     }
 }
